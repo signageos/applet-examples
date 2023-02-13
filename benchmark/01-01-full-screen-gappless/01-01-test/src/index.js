5eefe07dd89a6e050322121025a66f12242c3673
Garrett <garrett.grim@signageos.io>
2023-02-13 13:01:15 +0100

    first tests added 01-01

---
+++ benchmark/01-01-full-screen-gappless/01-01-test/src/index.js
@@ -0,0 +1,102 @@
+
+require('./index.css');
+
+import sos from '@signageos/front-applet';
+
+// Wait on sos data are ready (https://docs.signageos.io/api/js/content/latest/js-applet-basics#onready)
+sos.onReady().then(async function () {
+	//const contentElement = document.getElementById('root');
+	console.log('sOS is ready');
+
+	async function startApplet() {
+
+		// Load static files
+		let files = [
+			{
+			  "uri": "https://demo.signageos.io/benchmark/styles.css?v=1.0.0",	
+			  "uid": "styles.css-v1.0.0",
+				"type": sos.offline.types.css,
+				"flags": [sos.offline.flags.append(document.head)]
+			}
+		];
+		await sos.offline.addFiles(files);
+
+		const titleElement = document.getElementById('title');
+		const displayInfoElement = document.getElementById('displayInfo');
+		const indexElement = document.getElementById('index');
+		const htmlContentElement = document.getElementById('htmlContent');
+
+		await sos.onReady(); // Wait until signageOS app is loaded and ready
+		
+		//Get display details
+		try {
+			const fw = await sos.management.firmware.getVersion();
+			const pl = await sos.management.app.getType();
+			const model = await sos.management.getModel();
+			const displayInfo = document.getElementById('displayInfo');
+			displayInfo.innerHTML = "Platform: " + pl + " Model: " + model + " FW: " + fw;
+		} catch (e) {
+			console.log(e);
+		}
+
+		// Videos that will be downloaded and prepared for playing
+		const videos = [
+			{
+				uid: 'video-1.mp4',
+				uri: 'https://static.signageos.io/assets/test-videos-circle_AME/circle_15s_1920x1080_c32f55b932a8df2e39265b04a90d9837.mp4'
+			},
+			{
+				uid: 'video-2.mp4',
+				uri: 'https://static.signageos.io/assets/test-videos-03_AME/video-test-03_15s_1920x1080_2fe7b039750a134aeac1c0a515710007.mp4'
+			},
+			{
+			 	uid: 'video-3.mp4',
+			 	uri: 'https://static.signageos.io/assets/test-videos-04_AME/video-test-04_15s_1920x1080_88f90af5fa281d7efb8eae17848e71d9.mp4'
+			},
+			{
+			 	uid: 'video-4.mp4',
+			 	uri: 'https://static.signageos.io/assets/test-videos-05_AME/video-test-05_15s_1920x1080_0f110996c51d8c77933b1ab9fecfb61f.mp4'
+			},
+			{
+				uid: 'video-5.mp4',
+				uri: 'https://static.signageos.io/assets/test-videos-06_AME/video-test-06_15s_1920x1080_6bad9be8f365760ae505dab34582978b.mp4'
+			},
+		];
+
+		for (const video of videos) {
+			// Store video to offline storage (https://docs.signageos.io/api/js/js-offline-cache-media-files)
+			  indexElement.innerHTML = "Downloading: " + video.uri;
+			const { filePath } = await sos.offline.cache.loadOrSaveFile(video.uid, video.uri);
+			video.filePath = filePath;
+			indexElement.innerHTML = "Downloaded: " + video.filePath;
+		}
+
+		// Set video properties for proper play on screen
+		for (const video of videos) {
+			video.arguments = [video.filePath, 0, 0, 1920, 1080];
+		}
+		titleElement.innerHTML = '';
+		displayInfoElement.innerHTML = '';
+		indexElement.innerHTML = 'Ready to playback';
+		htmlContentElement.innerHTML = '';
+	  
+	  for (let i = 0; true; i = (i + 1) % videos.length) {
+			  const previousVideo = videos[(i + videos.length - 1) % videos.length];
+			  const currentVideo = videos[i];
+			  const nextVideo = videos[(i + 1) % videos.length];
+
+			  // Videos are identificated by URI & coordination together (https://docs.signageos.io/api/js/js-video)
+			  await sos.video.play(...currentVideo.arguments);
+			  currentVideo.playing = true;
+			  if (previousVideo.playing) {
+				  await sos.video.stop(...previousVideo.arguments);
+				  previousVideo.playing = false;
+			  }
+			await sos.video.prepare(...nextVideo.arguments); // Before the first video end, prepare the second one
+			await sos.video.onceEnded(...currentVideo.arguments); // Wait until video ends
+		}
+					  
+	}
+	// Needed check for distinction between sos open and platform in box
+	startApplet();
+});
---
+++ benchmark/01-01-full-screen-gappless/01-01-test/test/checkContent.spec.js
@@ -0,0 +1,74 @@
+const { waitUntil } = require('@signageos/sdk');
+const should = require('should');
+const { setupPlayerTiming, cleanTimings } = require("../lib/helper");
+const { appletUid, appletVersion } = require("../lib/general");
+
+describe('Check content', () => {
+
+	/** Current instance of running applet on the device */
+	let currentTiming;
+	beforeEach(async function () {
+		// Prepare timing on device with current applet and version
+		const { timing } = await setupPlayerTiming(appletUid, appletVersion);
+		currentTiming = timing;
+
+		// We have to wait until applet is loaded and ready
+		// await waitUntil(async () => {
+		// 	await currentTiming.onLoaded;
+		// 	const consoleLogs = await currentTiming.console.log.getAll();
+		// 	// The applet is ready when it logs "sOS is ready" to the console
+		// 	should(consoleLogs).containEql('sOS is ready');
+		// }, 60000);
+	});
+
+	afterEach(async function () {
+		// We clean up device applets after each test
+		await cleanTimings();
+	});
+
+
+	it("each video playing", async function () {
+		await waitUntil(async () => {
+
+			const playingVideos = await currentTiming.video.play.getAll();
+			const videos = playingVideos.map(v => v.uri)
+			const reg1 = /.*video-1.mp4/gm;
+			const reg2 = /.*video-2.mp4/gm;
+			const reg3 = /.*video-3.mp4/gm;
+			const reg4 = /.*video-4.mp4/gm;
+			const reg5 = /.*video-5.mp4/gm;
+
+			const matchRegex1 = videos.some(video => reg1.test(video))
+			should(matchRegex1).equal(true);
+			should(playingVideos[0].width).equal(1920);
+			should(playingVideos[0].height).equal(1080);
+		
+			const matchRegex2 = videos.some(video => reg2.test(video))
+			should(matchRegex2).equal(true);
+
+			should(playingVideos[1].width).equal(1920);
+			should(playingVideos[1].height).equal(1080);
+	
+			const matchRegex3 = videos.some(video => reg3.test(video))
+			should(matchRegex3).equal(true);
+			should(playingVideos[2].width).equal(1920);
+			should(playingVideos[2].height).equal(1080);
+
+
+			const matchRegex4 = videos.some(video => reg4.test(video))
+			should(matchRegex4).equal(true);
+			should(playingVideos[3].width).equal(1920);
+			should(playingVideos[3].height).equal(1080);
+		
+			const matchRegex5 = videos.some(video => reg5.test(video))
+			should(matchRegex5).equal(true);
+			should(playingVideos[4].width).equal(1920);
+			should(playingVideos[4].height).equal(1080);
+		
+			if (matchRegex1 && matchRegex2 && matchRegex3 && matchRegex4 && matchRegex5) {
+				console.log("All Videos played correctly")
+			}
+		}, 120000);
+		// Now test passed
+	});
+});

4d960b2f74a79ee8562c95b13e870833f07f72a9
Garrett <garrett.grim@signageos.io>
2023-03-07 08:10:23 +0100

    applet 03-01-test created with test

---
+++ benchmark/03-01-2-videos-overflow/03-01-test/test/checkContent.spec.js
@@ -0,0 +1,46 @@
+const { waitUntil } = require('@signageos/sdk');
+const should = require('should');
+const { setupPlayerTiming, cleanTimings } = require("../lib/helper");
+const { appletUid, appletVersion } = require("../lib/general");
+
+describe('Check content', () => {
+
+	/** Current instance of running applet on the device */
+	let currentTiming;
+	beforeEach(async function () {
+		// Prepare timing on device with current applet and version
+		const { timing } = await setupPlayerTiming(appletUid, appletVersion);
+		currentTiming = timing;
+	});
+
+	afterEach(async function () {
+		// We clean up device applets after each test
+		await cleanTimings();
+	});
+
+
+	it("each video playing", async function () {
+
+		await waitUntil(async () => {
+
+		const playingVideos = await currentTiming.video.play.getAll();
+		const videos = playingVideos.map(v => v.uri)
+
+		const reg1 = /.*video-2/gm;
+		const reg2 = /.*video-6/gm;
+
+		const matchRegex1 = videos.some(video => reg1.test(video))
+		should(matchRegex1).equal(true);
+	
+
+		const matchRegex2 = videos.some(video => reg2.test(video))
+		should(matchRegex2).equal(true);
+
+
+		 if (matchRegex1 && matchRegex2 ) {
+			console.log("All Videos played correctly")
+		}
+	}, 120000);
+	// Now test passed
+	});
+});

c94cf3fed81a95bac952389b2da7a6f3202d2358
Garrett <garrett.grim@signageos.io>
2023-04-03 08:31:59 +0200

    added check for additional videos played, added check of storing videos, however, there is problem with async process - all videoes show as stored, except the first one

--- benchmark/03-01-2-videos-overflow/03-01-test/test/checkContent.spec.js
+++ benchmark/03-01-2-videos-overflow/03-01-test/test/checkContent.spec.js
@@ -19,6 +19,87 @@ describe('Check content', () => {
 	});
 
 
+	it("Video 1 stored", async function () {
+
+		// check that device content has been loaded to the chache memory
+		await waitUntil(async () => {
+			const files = await currentTiming.offline.cache.listFiles();
+			console.log("video 1 before assertion");
+			should(files).containEql("video-2");
+			console.log("video 1 after assertion");
+		}, 900000);
+	});
+
+	it("Video 2 stored", async function () {
+
+		// check that device content has been loaded to the chache memory
+		await waitUntil(async () => {
+			const files = await currentTiming.offline.cache.listFiles();
+			console.log("video 2 before assertion");
+			should(files).containEql("video-4");
+			console.log("video 2 after assertion");
+		}, 900000);
+	});
+
+	it("Video 3 stored", async function () {
+
+		// check that device content has been loaded to the chache memory
+		await waitUntil(async () => {
+			const files = await currentTiming.offline.cache.listFiles();
+			console.log("video 3 before assertion");
+			should(files).containEql("video-5");
+			console.log("video 3 after assertion");
+		}, 900000);
+	});
+
+	it("Video 4 stored", async function () {
+
+		// check that device content has been loaded to the chache memory
+		await waitUntil(async () => {
+			const files = await currentTiming.offline.cache.listFiles();
+			console.log("video 4 before assertion");
+			should(files).containEql("video-6");
+			console.log("video 4 after assertion");
+		}, 900000);
+	});
+
+	it("Video 5 stored", async function () {
+
+		// check that device content has been loaded to the chache memory
+		await waitUntil(async () => {
+			const files = await currentTiming.offline.cache.listFiles();
+			console.log("video 5 before assertion");
+			should(files).containEql("video-7");
+			console.log("video 5 after assertion");
+		}, 900000);
+	});
+
+	it("Video 6 stored", async function () {
+
+		// check that device content has been loaded to the chache memory
+		await waitUntil(async () => {
+			const files = await currentTiming.offline.cache.listFiles();
+			console.log("video 6 before assertion");
+			should(files).containEql("video-9");
+			console.log("video 6 after assertion");
+		}, 900000);
+	});
+
+	// // it("Device stored video content", async function () {
+
+	// // 	// check that device content has been loaded to the chache memory
+	// // 	await waitUntil(async () => {
+	// // 		const files = await currentTiming.offline.cache.listFiles();
+	// // 		console.log(files);
+	// // 		should(files).containEql("video-2");
+	// // 		should(files).containEql("video-4");
+	// // 		// should(files).containEql("video-5");
+	// // 		// should(files).containEql("video-6");
+	// // 		// should(files).containEql("video-7");
+	// // 		// should(files).containEql("video-9");
+	// // 	}, 30000);
+	// // });
+
 	it("each video playing", async function () {
 
 		await waitUntil(async () => {
@@ -26,11 +107,13 @@ describe('Check content', () => {
 		const playingVideos = await currentTiming.video.play.getAll();
 		const videos = playingVideos.map(v => v.uri)
 
-		const reg1 = /.*video-2/gm;
-		const reg2 = /.*video-6/gm;
+		const reg1 = /.*video-4/gm;
+		const reg2 = /.*video-7/gm;
+		const reg3 = /.*video-5/gm;
+		const reg4 = /.*video-9/gm;
+		const reg5 = /.*video-2/gm;
+		const reg6 = /.*video-6/gm;
 
-
-		console.log(videos)
 		const matchRegex1 = videos.some(video => reg1.test(video))
 		should(matchRegex1).equal(true);
 	
@@ -38,8 +121,20 @@ describe('Check content', () => {
 		const matchRegex2 = videos.some(video => reg2.test(video))
 		should(matchRegex2).equal(true);
 
+		const matchRegex3 = videos.some(video => reg3.test(video))
+		should(matchRegex3).equal(true);
 
-		 if (matchRegex1 && matchRegex2 ) {
+		const matchRegex4 = videos.some(video => reg4.test(video))
+		should(matchRegex4).equal(true);
+
+		const matchRegex5 = videos.some(video => reg5.test(video))
+		should(matchRegex5).equal(true);
+
+		const matchRegex6 = videos.some(video => reg6.test(video))
+		should(matchRegex6).equal(true);
+
+
+		 if (matchRegex1 && matchRegex2 && matchRegex3  && matchRegex4  && matchRegex5 && matchRegex6 ) {
 			console.log("All Videos played correctly")
 		}
 	}, 120000);
